ea2ddb67d01d2679e6d5cefbe20a793d61cc39ea
jkatindi <43222970+jkatindi@users.noreply.github.com>
2026-02-02 02:01:24 +0100

    cluster-master-ok

--- scripts/cluster-install.sh
+++ scripts/cluster-install.sh
@@ -2,29 +2,25 @@
 
 set -euo pipefail
 
-TOKEN="4GG5YUmpDFk9vY8rZT7mj"
+TOKEN="m3v8j9.q1w2e3r4t5y6u7i8"
 HOSTNAME=$(hostname)
 POD_CIDR="10.244.0.0/16" # Flannel default
 MASTER_IP=$(hostname | awk '{print $2}')
 echo "[cluster-install] Master node hostname: $HOSTNAME, IP: $MASTER_IP"
 
 echo "[cluster-install] Configure containerd (SystemdCgroup=true)"
-sudo mkdir -p /etc/containerd
-if [ ! -f /etc/containerd/config.toml ]; then
-  containerd config default | sudo tee /etc/containerd/config.toml >/dev/null
-fi
-sudo sed -i 's/^\(\s*SystemdCgroup = \)false/\1true/' /etc/containerd/config.toml || true
-sudo systemctl enable --now containerd
 
-echo "[cluster-install] Enable kubelet service"
-sudo systemctl enable kubelet || true
+sudo rm /etc/containerd/config.toml
+sudo systemctl restart containerd
+sudo mkdir -p /etc/containerd
+containerd config default | sudo tee /etc/containerd/config.toml >/dev/null
+sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/config.toml
 
 if [ -f /etc/kubernetes/admin.conf ]; then
   echo "[cluster-install] Cluster appears initialized already. Skipping kubeadm init."
 else
   echo "[cluster-install] Initializing control plane with kubeadm..."
-  sudo kubeadm init --apiserver-advertise-address=$MASTER_IP \
-       --pod-network-cidr=$POD_CIDR --token="$TOKEN"
+  sudo kubeadm init  --apiserver-advertise-address=$MASTER_IP  --ignore-preflight-errors=all --pod-network-cidr=$POD_CIDR --token="$TOKEN"
 fi
 
 echo "[cluster-install] Setting kubeconfig for current user"
--- scripts/node-join.sh
+++ scripts/node-join.sh
@@ -4,27 +4,23 @@ set -euo pipefail
 
 HOSTNAME=$(hostname)
 MASTER_IP="192.168.10.2"
-TOKEN="4GG5YUmpDFk9vY8rZT7mj"
+TOKEN="m3v8j9.q1w2e3r4t5y6u7i8"
 echo "[node-join] Worker hostname: $HOSTNAME"
 
 echo "[node-join] Configure containerd (SystemdCgroup=true)"
+udo rm /etc/containerd/config.toml
+sudo systemctl restart containerd
 sudo mkdir -p /etc/containerd
-if [ ! -f /etc/containerd/config.toml ]; then
-  containerd config default | sudo tee /etc/containerd/config.toml >/dev/null
-fi
-#sudo sed -i 's/^\(\s*SystemdCgroup = \)false/\1true/' /etc/containerd/config.toml || true
-sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/' /etc/containerd/config.toml
-
-sudo systemctl enable --now containerd
-
+containerd config default | sudo tee /etc/containerd/config.toml >/dev/null
+sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/config.toml
+sudo systemctl restart containerd
 echo "[node-join] Reset any previous kubeadm state"
 sudo kubeadm reset -f || true
 
 echo "[node-join] Enable kubelet service"
 sudo systemctl enable kubelet || true
 
-kubeadm  join --ignore-preflight-errors=all  --token="$TOKEN"  $MASTER_IP:6443 \
-  --discovery-token-unsafe-skip-ca-verification
+kubeadm  join --ignore-preflight-errors=all  --token="$TOKEN"  $MASTER_IP:6443 --discovery-token-unsafe-skip-ca-verification
 
 echo "[node-join] Restarting kubelet"
 sudo systemctl restart kubelet || true
